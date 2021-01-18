95e99926cbc9745aefce93808c9f88d99095aa35
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-01-18 10:42:04 +0100

    Movement

--- Assets/Scripts/Gameplay/Survivor/SurvivorController.cs
+++ Assets/Scripts/Gameplay/Survivor/SurvivorController.cs
@@ -4,47 +4,37 @@ using UnityEngine;
 
 public class SurvivorController : MonoBehaviour
 {
-    Rigidbody rb;
-
-    float minRotY = -75f;
-    float maxRotY = 75F;
-    float lookSpeed = 3.0f;
-    public float rotY;
-    public float rotX;
-    public float speed;
-    public new Camera camera;
-    public Vector3 moveDirection = Vector3.zero;
+    CharacterController cc;
+    public float speed = 1f;
+    Vector3 moveDirection = Vector3.zero;
     private float horizontal;
     private float vertical;
 
-    private void Start()
+    private void Awake()
+    {        
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
     {
-        rb = GetComponent<Rigidbody>();
+        JODSInput.Controls.Survivor.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+    }
+
+    private void OnDisable()
+    {
+        JODSInput.Controls.Survivor.Movement.performed -= ctx => Move(ctx.ReadValue<Vector2>());
     }
 
     private void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
-
-
-        //Camera controls
-        rotY += Input.GetAxis("Mouse Y") * lookSpeed;
-        rotX += Input.GetAxis("Mouse X") * lookSpeed;
-        rotY = Mathf.Clamp(rotY, minRotY, maxRotY);
-        camera.transform.rotation = Quaternion.Euler(-rotY, rotX, 0f);
-        transform.rotation = Quaternion.Euler(0f, rotX, 0f);
-
-        //Movement
-        moveDirection = new Vector3(horizontal, 0.0f, vertical);
-        moveDirection = transform.TransformDirection(moveDirection);
-
-        //Fixes the issue of moving faster if going forwards and sideways.
-        if (moveDirection.magnitude > 1)
-        {
-            moveDirection.Normalize();
-        }
-        moveDirection = moveDirection * speed;
-        rb.MovePosition(transform.position + moveDirection);
+        moveDirection = transform.TransformDirection(new Vector3(horizontal, 0.0f, vertical)) * speed;
+        cc.Move(moveDirection);
     }
+    private void Move(Vector2 moveValues)
+    {
+        horizontal = moveValues.x;
+        vertical = moveValues.y;
+        Debug.Log(moveValues);
+    }
+
 }
