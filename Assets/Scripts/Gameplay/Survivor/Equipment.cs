b727804a9d1b6812cf18c973d28b1ac23c00f7a0
Jontan1337 <jonathangf1337@gmail.com>
2021-01-20 13:27:02 +0100

    Equipment system WIP

--- Assets/Scripts/Gameplay/Survivor/Equipment.cs
+++ Assets/Scripts/Gameplay/Survivor/Equipment.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Equipment : MonoBehaviour
 {
-    private GameObject primary = null;
-    private GameObject secondary = null;
-    private GameObject throwable = null;
-    private GameObject meds = null;
+    public EquipmentBar selectedEquipmentBar = null;
 
+    public List<EquipmentBar> EquipmentBars = new List<EquipmentBar>();
 
+    private int equipmentBarsCount = 0;
+    private void Awake()
+    {
+        equipmentBarsCount = EquipmentBars.Count;
+    }
+
+    public void Equip(GameObject equipment, EquipmentType equipmentType)
+    {
+        if (selectedEquipmentBar.equipment == null)
+        {
+            selectedEquipmentBar.Equip(equipment, equipmentType);
+        }
+        else
+        {
+            for (int i = 0; i < equipmentBarsCount; i++)
+            {
+                if (EquipmentBars[i].Equip(equipment, equipmentType))
+                {
+
+                }
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/Gameplay/Survivor/EquipmentBar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class EquipmentBar : MonoBehaviour
+{
+    public GameObject equipment;
+    public string barName = "Bar Name";
+
+    [SerializeField]
+    private EquipmentType equipmentType;
+
+    public bool Equip(GameObject equipment, EquipmentType equipmentType)
+    {
+        if (equipmentType == this.equipmentType)
+        {
+            this.equipment = equipment;
+            return true;
+        }
+        return false;
+    }
+}
