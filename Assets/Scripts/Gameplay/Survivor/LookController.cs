ede7806a35b7ece55950990b3c09dbacfd2f2e86
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-01-20 12:21:33 +0100

    Sensitivity

--- Assets/Scripts/Gameplay/Survivor/LookController.cs
+++ Assets/Scripts/Gameplay/Survivor/LookController.cs
@@ -8,15 +8,14 @@ public class LookController : MonoBehaviour
     float maxRotY = 75F;
     public float rotY;
     public float rotX;
-    float lookSpeed = 3.0f;
-    float sensitivity = 1f;
+    public float sensitivity;
     public new Camera camera;
 
     void Update()
     {
-        rotY += Input.GetAxis("Mouse Y") * lookSpeed * sensitivity;
-        rotX += Input.GetAxis("Mouse X") * lookSpeed * sensitivity;
-        transform.Rotate(0, lookSpeed * Input.GetAxis("Mouse X") * sensitivity, 0);
+        rotY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotX += Input.GetAxis("Mouse X") * sensitivity;
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
         rotY = Mathf.Clamp(rotY, minRotY, maxRotY);
         camera.transform.rotation = Quaternion.Euler(-rotY, rotX, 0f);
     }
