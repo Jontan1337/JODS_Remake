644acd80cd315c518ae457361a62a1d312c8a8eb
VodkaZombie <45755306+VodkaZombie@users.noreply.github.com>
2021-02-10 11:52:52 +0100

    b

---
+++ Assets/Scripts/Gameplay/Master/FloorIndicator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorIndicator : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    [SerializeField] private Color deselected;
+    [SerializeField] private Color selected;
+
+    public void Select(bool select)
+    {
+        image.color = select ? selected : deselected;
+    }
+}
--- Assets/Scripts/Gameplay/Master/Master_TopdownController.cs
+++ Assets/Scripts/Gameplay/Master/Master_TopdownController.cs
@@ -12,50 +12,26 @@ public class Master_TopdownController : MonoBehaviour
     [SerializeField] private int positionChange = 5;
     [SerializeField] private int amountOfFloors = 0;
 
+    [Header("UI")]
+    [SerializeField] private Transform floorUIContainer;
+    [SerializeField] private GameObject floorIndicatorPrefab;
+    private List<FloorIndicator> floorIndicators = new List<FloorIndicator>();
+
     private float horizontal; // These variables are used to move the player. 
     private float vertical; // They store the player's input values.
 
-    private void OnEnable()
-    {
-        /*
-        //Shift Input
-        JODSInput.Controls.Master.Shift.started += ctx => ShiftButton(true);
-        JODSInput.Controls.Master.Shift.canceled += ctx => ShiftButton(false);
+    private bool shift = false; //Used to check if the player is holding down the shift key
 
-        // Movement Input
-        JODSInput.Controls.Master.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
-
-        //Floor Input
-        JODSInput.Controls.Master.FloorDown.performed += ctx => ChangeFloor(false);
-        JODSInput.Controls.Master.FloorUp.performed += ctx => ChangeFloor(true);
-        */
-    }
-
-    private void OnDisable()
-    {
-        /*
-        //Shift Input
-        JODSInput.Controls.Master.Shift.started -= ctx => ShiftButton(true);
-        JODSInput.Controls.Master.Shift.canceled -= ctx => ShiftButton(false);
-
-        // Movement Input
-        JODSInput.Controls.Master.Movement.performed -= ctx => Move(ctx.ReadValue<Vector2>());
-
-        //Floor Input
-        JODSInput.Controls.Master.FloorDown.performed -= ctx => ChangeFloor(false);
-        JODSInput.Controls.Master.FloorUp.performed -= ctx => ChangeFloor(true);
-        */
-    }
-    [Space]
-    [SerializeField] private bool shift = false;
     private void ShiftButton(bool down)
     {
-        shift = down;
+        shift = down; //yep
     }
 
     private void Start()
     {
         cam = GetComponent<Camera>();
+
+
 
         //Default floor stuff
 
@@ -63,10 +39,20 @@ public class Master_TopdownController : MonoBehaviour
         amountOfFloors = GameObject.FindGameObjectsWithTag("Floor").Length;
         Debug.Log($"There are {amountOfFloors} floors on this map");
 
+        //Instantiate floor indicators for each floor on the map
+        for (int i = 0; i < amountOfFloors; i++)
+        {
+            floorIndicators.Add(Instantiate(floorIndicatorPrefab, floorUIContainer).GetComponent<FloorIndicator>());
+        }
+
         //Start on a random floor
-        currentFloor = Random.Range(1, amountOfFloors + 1);
+        int rndFloor = Random.Range(1, amountOfFloors + 1);
+        currentFloor = rndFloor;
+        Debug.Log($"Current floor : {currentFloor}");
 
         ChangeFloor();
+
+
 
         //Input stuff
 
@@ -106,6 +92,11 @@ public class Master_TopdownController : MonoBehaviour
     #region Floor Navigation
     void ChangeFloor()
     {
+        for (int i = 0; i < floorIndicators.Count; i++)
+        {
+            floorIndicators[i].Select(i == currentFloor - 1);
+        }
+
         cam.transform.position = new Vector3(
             cam.transform.position.x,
             positionChange * currentFloor - 0.05f,
@@ -115,7 +106,12 @@ public class Master_TopdownController : MonoBehaviour
     {
         print("Changing floor " + (up ? "up" : "down"));
 
-        currentFloor = Mathf.Clamp(currentFloor += up ? -1 : 1, 1, amountOfFloors); //This needs fixin
+        currentFloor = Mathf.Clamp(currentFloor += up ? 1 : -1, 1, amountOfFloors); //This needs fixin
+
+        for (int i = 0; i < floorIndicators.Count; i++)
+        {
+            floorIndicators[i].Select(i == currentFloor - 1);
+        }
 
         cam.transform.position = new Vector3(
             cam.transform.position.x,
