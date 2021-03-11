f94105d12c1605ffc3db51cc5424a65b2a08892e
VodkaZombie <45755306+VodkaZombie@users.noreply.github.com>
2021-03-11 11:15:13 +0100

    lobby and offline start

---
+++ Assets/Scripts/UI/OfflineButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OfflineButton : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoadName;
+
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene(sceneToLoadName);
+    }
+}
