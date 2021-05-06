1dff4650096561b58f3e79682db750e5773caf32
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-05-06 08:16:02 +0200

    place item ting

--- Assets/Scripts/Gameplay/Items/AutoTurret.cs
+++ Assets/Scripts/Gameplay/Items/AutoTurret.cs
@@ -3,67 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AutoTurret : NetworkBehaviour, IEquippable, IBindable, IInteractable
+
+
+public class AutoTurret : NetworkBehaviour
 {
-    private PlaceItem place;
-    private Coroutine placeHolderActive;
+
+
 
     private void Start()
     {
-        place = GetComponent<PlaceItem>();
+
     }
 
-    public string Name => gameObject.name;
 
-    public GameObject Item => gameObject;
 
-    public EquipmentType EquipmentType => EquipmentType.None;
-
-    private bool isInteractable = true;
-    public bool IsInteractable
-    {
-        get => isInteractable;
-        set => isInteractable = value;
-    }
-    public string ObjectName => throw new System.NotImplementedException();
-
-	public void Bind()
-    {
-        throw new System.NotImplementedException();
-    }
-    public void UnBind()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    [Server]
-    public void Svr_GiveAuthority(NetworkConnection conn)
-    {
-        netIdentity.AssignClientAuthority(conn);
-    }
-    [Server]
-    public void Svr_RemoveAuthority()
-    {
-        netIdentity.RemoveClientAuthority();
-    }
-    [Server]
-    public void Svr_Interact(GameObject interacter)
-	{
-        if (!IsInteractable) return;
-
-        // Equipment should be on a child object of the player.
-        Equipment equipment = interacter.GetComponentInChildren<Equipment>();
-
-        if (equipment != null)
-        {
-            Svr_GiveAuthority(interacter.GetComponent<NetworkIdentity>().connectionToClient);
-            equipment?.Svr_Equip(gameObject, EquipmentType);
-            IsInteractable = false;
-        }
-        else
-        {
-            // This should not be possible, but just to be absolutely sure.
-            Debug.LogWarning($"{interacter} does not have an Equipment component", this);
-        }
-    }
 }
