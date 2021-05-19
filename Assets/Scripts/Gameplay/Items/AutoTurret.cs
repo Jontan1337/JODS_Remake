02437a3addbc5a584b1bce1fc2de62eb491784f9
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-05-19 09:23:57 +0200

    turret ting

--- Assets/Scripts/Gameplay/Items/AutoTurret.cs
+++ Assets/Scripts/Gameplay/Items/AutoTurret.cs
@@ -7,14 +7,38 @@ using UnityEngine;
 
 public class AutoTurret : NetworkBehaviour
 {
+	[SerializeField]
+	private float range = 10000;
+	[SerializeField]
+	private LayerMask ignoreLayer;
+
+	private bool active = true;
+	private Collider[] targets;
+
+	private void Start()
+	{
+		StartCoroutine(TurretShoot());
+	}
+
+	IEnumerator TurretShoot()
+	{
+		Debug.DrawRay(transform.position, transform.forward * range);
+
+		print("yeeee1");
+		while (active)
+		{
+			targets = Physics.OverlapSphere(transform.position, range, ~ignoreLayer);
+			for (int i = 0; i < targets.Length; i++)
+			{
 
 
 
-    private void Start()
-    {
+				//print(Vector3.Distance(targets[i].transform.position, gameObject.transform.position));
+				//print(targets[i].name);
 
-    }
+			}
+			yield return new WaitForSeconds(1);
+		}
 
-
-
+	}
 }
