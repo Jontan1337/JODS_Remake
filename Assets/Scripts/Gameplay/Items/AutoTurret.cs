47306d142b27806dbf4451fccb4a9b1fb0ce5d28
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-05-21 12:19:44 +0200

    Turret tiiing

--- Assets/Scripts/Gameplay/Items/AutoTurret.cs
+++ Assets/Scripts/Gameplay/Items/AutoTurret.cs
@@ -7,41 +7,55 @@ using UnityEngine;
 
 public class AutoTurret : NetworkBehaviour, IDamagable
 {
+
+	[Header("Stats")]
 	[SerializeField]
 	private float range = 10000;
 	[SerializeField]
+	private float fireRate = 0.1f;
+	[SerializeField]
 	private int damage = 20;
+	[SerializeField]
+	private int duration = 20;
+	[SerializeField]
+	private int health = 200;
+
+
+
+	[Header("References")]
+	[SerializeField]
+	private Transform target = null;
+	[SerializeField]
+	private Transform swivel = null;
+	[SerializeField]
+	private Transform pivot = null;
+	[SerializeField]
+	private Transform barrel = null;
+
+	[Space]
 	[SerializeField]
 	private LayerMask ignoreLayer;
 
-	private bool active = true;
 	private Collider[] enemiesInRange;
-	[SerializeField]
-	private Transform target;
-	[SerializeField]
-	private Transform swivel;
-	[SerializeField]
-	private Transform pivot;
-	[SerializeField]
-	private Transform barrel;
-
-	bool isSearching = false;
-
-
+	private List<Collider> enemiesInSight;
+	private bool isSearching = false;
+	private bool isDead;
 
 
 	private void Start()
 	{
 		StartSearching();
+		StartCoroutine(Duration());
 	}
 
-	Coroutine TurretCo;
-	IEnumerator Turret()
+	#region Coroutines
+	Coroutine ShootIntervalCo;
+	IEnumerator ShootInterval()
 	{
 		while (true)
 		{
 			if (CanShoot()) Shoot();
-			yield return new WaitForSeconds(0.1f);
+			yield return new WaitForSeconds(fireRate);
 		}
 	}
 
@@ -68,14 +82,46 @@ public class AutoTurret : NetworkBehaviour, IDamagable
 			yield return null;
 		}
 	}
+
+	//IEnumerator RotateX()
+	//{
+	//	Quaternion lookRotation = Quaternion.LookRotation((target.position - cylinder.position));
+	//	swivel.rotation = Quaternion.Slerp(swivel.rotation, lookRotation, 3 * Time.deltaTime);
+	//	swivel.localEulerAngles = new Vector3(0, swivel.localEulerAngles.y, 0);
+	//	yield return null;
+	//}
+
+	IEnumerator Duration()
+	{
+		while (duration > 0)
+		{
+			duration--;
+
+			yield return new WaitForSeconds(1);
+		}
+		Die();
+	}
+
+	#endregion
+
+	#region Methods
 	void FindTarget()
 	{
-		//RaycastHit hit;
-		//Physics.Raycast();
+		enemiesInSight = new List<Collider>();
 		enemiesInRange = Physics.OverlapSphere(transform.position, range, ignoreLayer);
-		if (enemiesInRange[0])
+		RaycastHit hit;
+		foreach (Collider item in enemiesInRange)
 		{
-			NewTarget(enemiesInRange[0].transform);
+			Physics.Raycast(transform.position, (item.transform.position - transform.position), out hit);
+			if (hit.transform == item.transform)
+			{
+				print(hit.transform.name);
+				enemiesInSight.Add(item);
+			}
+		}
+		if (enemiesInSight.Count > 0 && enemiesInSight[0])
+		{
+			NewTarget(enemiesInSight[0].transform);
 		}
 	}
 	void Shoot()
@@ -96,14 +142,14 @@ public class AutoTurret : NetworkBehaviour, IDamagable
 			isSearching = false;
 		}
 		RotateYCo = StartCoroutine(RotateY());
-		TurretCo = StartCoroutine(Turret());
+		ShootIntervalCo = StartCoroutine(ShootInterval());
 	}
 
 	void LostTarget()
 	{
 		target = null;
 		StopCoroutine(RotateYCo);
-		StopCoroutine(TurretCo);
+		StopCoroutine(ShootIntervalCo);
 		StartSearching();
 	}
 
@@ -117,14 +163,33 @@ public class AutoTurret : NetworkBehaviour, IDamagable
 
 	bool CanShoot()
 	{
-		return true;
+		RaycastHit hit;
+		Debug.DrawRay(barrel.position, barrel.forward * 10, Color.red, 0.1f);
+		Physics.Raycast(barrel.position, barrel.forward, out hit, range, ignoreLayer);
+		if (hit.transform == target.transform)	return true;
+		return false;
 	}
+
+	void Die()
+	{
+		StopAllCoroutines();
+		Destroy(gameObject);
+	}
+
+	#endregion
+
 	#region IDamagable
 
 	public Teams Team => throw new System.NotImplementedException();
 	public void Svr_Damage(int damage, Transform target = null)
 	{
-		throw new System.NotImplementedException();
+		if (IsDead()) return;
+		health -= damage;
+		if (health <= 0)
+		{
+			isDead = true;
+			Die();
+		}
 	}
 
 	public int GetHealth()
@@ -132,9 +197,7 @@ public class AutoTurret : NetworkBehaviour, IDamagable
 		throw new System.NotImplementedException();
 	}
 
-	public bool IsDead()
-	{
-		throw new System.NotImplementedException();
-	}
+	public bool IsDead() => isDead;
+
 	#endregion
 }
