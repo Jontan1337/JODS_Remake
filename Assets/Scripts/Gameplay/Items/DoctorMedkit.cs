2e320f85eb7dcda66535ec0ea9102c3b13fd9c30
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2021-06-29 13:56:53 +0200

    THE DOCTOR IS IN

---
+++ Assets/Scripts/Gameplay/Items/DoctorMedkit.cs
@@ -0,0 +1,33 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoctorMedkit : EquipmentItem
+{
+
+	[SyncVar] private int uses = 4;
+	[SerializeField] private GameObject medKit;
+
+	//public override void Svr_Drop()
+	//{
+	//	base.Svr_Drop();
+
+	//}
+
+	[Server]
+	public override void Svr_Interact(GameObject interacter)
+	{
+		PlayerEquipment playerEquipment = interacter.GetComponentInChildren<PlayerEquipment>();
+
+		if (uses > 0)
+		{
+			GameObject newMedKit = Instantiate(medKit, transform.position, transform.rotation);
+			NetworkServer.Spawn(newMedKit);
+			//newMedKit.GetComponent<EquipmentItem>().Svr_Pickup(playerEquipment.playerHands, interacter.GetComponent<NetworkIdentity>().connectionToClient);
+			playerEquipment?.Svr_Equip(newMedKit, EquipmentType.Meds);
+			uses--;
+		}
+
+	}
+}
---
+++ Assets/Scripts/Gameplay/Survivor/SurvivorClasses/DoctorClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DoctorClass : SurvivorClass
+{
+	private PlayerEquipment playerEquipment;
+	private GameObject medKit;
+
+	public override void ActiveAbility()
+	{
+		EquipMedKit();
+		if (!medKit)
+		{
+		}
+
+		// Equipment slot 4 empty?
+		//if ()
+		//{
+		//}
+	}
+
+	[Server]
+	private void EquipMedKit()
+	{
+		medKit = Instantiate(abilityObject, transform.position, transform.rotation);
+		NetworkServer.Spawn(medKit);
+		playerEquipment = transform.parent.GetComponentInChildren<PlayerEquipment>();
+
+		medKit.GetComponent<EquipmentItem>().Svr_Pickup(playerEquipment.playerHands, connectionToClient);
+		playerEquipment?.Svr_Equip(medKit, EquipmentType.Meds);
+		GetComponentInParent<ActiveSClass>().StartAbilityCooldownCo();
+	}
+}
