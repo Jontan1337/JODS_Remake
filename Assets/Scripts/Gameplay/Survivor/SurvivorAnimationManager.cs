afa1dff7e028ca5639dc24b8bc80af73bc244795
VodkaZombie <45755306+VodkaZombie@users.noreply.github.com>
2021-11-02 14:07:16 +0100

    naimaort

---
+++ Assets/Scripts/Gameplay/Survivor/SurvivorAnimationManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class SurvivorAnimationManager : NetworkBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasAuthority) return;
+        anim = GetComponent<Animator>();
+    }
+
+    public void HasWeaponAnimation(bool enable)
+    {
+        anim.SetBool("HasWeapon", enable);
+
+        //Do some IK stuff here to get the hands to fit onto the weapon.
+        Debug.LogWarning("IK stuff here");
+    }
+}
