a9fb9793d69493918a0b9324d660a11830669d16
VodkaZombie <45755306+VodkaZombie@users.noreply.github.com>
2022-01-17 14:54:45 +0100

    zm

--- Assets/Scripts/Gameplay/Status Effects/Movement/MovementSpeed.cs
+++ Assets/Scripts/Gameplay/Status Effects/Movement/MovementSpeed.cs
@@ -6,17 +6,20 @@ public class MovementSpeed : StatusEffect
 {
     private SurvivorController sClass;
     private MovementSpeedSO movementSpeed;
-    private float defaultSpeed;
+    private float defaultWalkSpeed;
+    private float defaultSprintSpeed;
     public MovementSpeed(StatusEffectSO effect, GameObject obj) : base(effect, obj)
     {
         sClass = obj.GetComponent<SurvivorController>();
         movementSpeed = (MovementSpeedSO) effect;
-        defaultSpeed = sClass.walkSpeedMultiplier;
+        defaultWalkSpeed = sClass.walkSpeedMultiplier;
+        defaultSprintSpeed = sClass.sprintSpeedMultiplier;
     }
     public override void OnEffectApplied()
     {
-        sClass.walkSpeedMultiplier += defaultSpeed * movementSpeed.speedModifier;
-    }
+        sClass.walkSpeedMultiplier += defaultWalkSpeed * movementSpeed.speedModifier;
+        sClass.sprintSpeedMultiplier += defaultSprintSpeed * movementSpeed.speedModifier;
+}
     public override void ApplyEffect(int? amount)
     {
 
@@ -24,7 +27,8 @@ public class MovementSpeed : StatusEffect
 
     public override void End()
     {
-        sClass.walkSpeedMultiplier = defaultSpeed;
+        sClass.walkSpeedMultiplier = defaultWalkSpeed;
+        sClass.sprintSpeedMultiplier = defaultSprintSpeed;
     }
 
     public override void Tick()
