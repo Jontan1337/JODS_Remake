92842299ea886b8d405d6369583e6c9d05a22e83
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2022-02-03 14:54:31 +0100

    mine

--- Assets/Scripts/Gameplay/Items/Mine.cs
+++ Assets/Scripts/Gameplay/Items/Mine.cs
@@ -7,12 +7,14 @@ public class Mine : NetworkBehaviour, IPlaceable
 {
     [SerializeField] private bool armed = false;
     [SerializeField] private SphereCollider col;
+    [SerializeField] private LayerMask mask = 0;
+
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.LogError("OTHER EXPLOSIONS AFFECT MINE! " +
             "IF YOU SHOOT ROCKET NEAR DEPLOYED MINE, MINE GOES FLYING.");
-        if (armed && other.gameObject.layer == 10) // Why check layer? ONLY Layer 10 should even be able to interact with this, so there is no need to check.
+        if (armed && mask == (mask | (1 << other.gameObject.layer))) // Why check layer? ONLY Layer 10 should even be able to interact with this, so there is no need to check.
         {   //This onTriggerEnter triggers when it is deployed, meaning it interacts with something that ISN'T layer 10. This should not happen.
             //Fix.
             //If the issue is that the mine will fall through the world if it can only interact with layer 10, then find a workaround (1 world collider 1 trigger?). Don't check layer.
