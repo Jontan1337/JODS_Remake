7260c6e10534563af3b58a6c407e0ddbfed2b0b6
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2022-02-16 14:53:17 +0100

    syringe

--- Assets/Scripts/Gameplay/Survivor/SurvivorClasses/Doctor/Syringe.cs
+++ Assets/Scripts/Gameplay/Survivor/SurvivorClasses/Doctor/Syringe.cs
@@ -5,35 +5,40 @@ using Mirror;
 
 public class Syringe : Projectile
 {
-	public override void Start()
-	{
-		base.Start();
-		objectPoolTag = Tags.Syringe;
-		transform.Rotate(new Vector3(90, 0, 0));
-		StartCoroutine(LifeTime());
-	}
-	[Server]
-	public override void OnHit(Collision hit)
-	{
-		base.OnHit(hit);
-		IDamagable idmg = hit.collider.GetComponent<IDamagable>();
-		if (idmg?.Team == Teams.Player)
-		{
-			foreach (StatusEffectSO statusEffectToApply in statusEffectsToApply)
-			{
-				hit.collider.transform.root.gameObject.GetComponent<StatusEffectManager>()?.Svr_ApplyStatusEffect(statusEffectToApply.ApplyEffect(hit.collider.transform.root.gameObject));
-			}
-		}
-		else
-		{
-			idmg?.Svr_Damage(200, owner);
-		}
-	}
+    [SerializeField] StatusEffectSO statusEffectToRemove = null;
+    public override void Start()
+    {
+        base.Start();
+        objectPoolTag = Tags.Syringe;
+        transform.Rotate(new Vector3(90, 0, 0));
+        StartCoroutine(LifeTime());
+    }
+    [Server]
+    public override void OnHit(Collision hit)
+    {
+        base.OnHit(hit);
+        IDamagable idmg = hit.collider.GetComponent<IDamagable>();
+        if (idmg?.Team == Teams.Player)
+        {
+            foreach (StatusEffectSO statusEffectToApply in statusEffectsToApply)
+            {
+                hit.collider.transform.root.gameObject.GetComponent<StatusEffectManager>()?.Svr_ApplyStatusEffect(statusEffectToApply.ApplyEffect(hit.collider.transform.root.gameObject));
+            }
+        }
+        else
+        {
+            idmg?.Svr_Damage(200, owner);
+        }
+        if (statusEffectToRemove.name == "Burn")
+        {
+            hit.collider.transform.root.gameObject.GetComponent<StatusEffectManager>()?.Svr_RemoveStatusEffect(statusEffectToRemove);
+        }
+    }
 
-	IEnumerator LifeTime()
-	{
-		yield return new WaitForSeconds(5f);
-		Destroy();
-	}
+    IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy();
+    }
 }
 
