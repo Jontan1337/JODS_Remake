332a23bb20f98d4ae4530fadfb394b8bca6ad8b7
Jontan1337 <jonathangf1337@gmail.com>
2022-03-09 14:39:23 +0100

    Buffed some weapons. Updated hotbar slots.

--- Assets/Scripts/UI/In game/HotbarSlot.cs
+++ Assets/Scripts/UI/In game/HotbarSlot.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HotbarSlot : MonoBehaviour
 {
-    [SerializeField] private TMP_Text textItemName;
-    [SerializeField] private TMP_Text textItemType;
+    [SerializeField] private Image hotbarItemImage = null;
+    [SerializeField] private TMP_Text textItemName = null;
+    [SerializeField] private TMP_Text textItemType = null;
 
+    public void SetItemImage(Sprite sprite)
+    {
+        hotbarItemImage.sprite = sprite;
+    }
+    public void ToggleItemImage()
+    {
+        hotbarItemImage.enabled = !hotbarItemImage.enabled;
+    }
     public void SetItemName(string itemName)
     {
         textItemName.text = itemName;
