7ffed3dbfe819d95633482bb29fe3bf9a6756129
Jonathan Frank <jofr@pc-tec.local>
2022-03-11 09:46:06 +0100

    Custom time wait thing that doesn't require coroutines

---
+++ Assets/Scripts/Managers/JODSTime.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public class JODSTime : MonoBehaviour
+{
+    public static async Task WaitTime(float duration)
+    {
+        float targetTime = Time.time + duration;
+        while (Time.time < targetTime)
+        {
+            await Task.Yield();
+        }
+    }
+    public static async Task WaitFrame()
+    {
+        await Task.Yield();
+    }
+}
