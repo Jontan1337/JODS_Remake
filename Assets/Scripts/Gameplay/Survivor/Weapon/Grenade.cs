4e24e5b26ef60f4a462d46b614fdc5097ad31bd5
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2022-03-29 14:58:53 +0200

    grenade stuff

--- Assets/Scripts/Gameplay/Survivor/Weapon/Grenade.cs
+++ Assets/Scripts/Gameplay/Survivor/Weapon/Grenade.cs
@@ -3,44 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class Grenade : NetworkBehaviour
+public class Grenade : Projectile
 {
-    public GameObject explosionEffect;
-
-    [SyncVar] public bool thrown;
-    public float delay = 3f;
-    public float destroyDelay = 10f;
-    public float blastRadius = 5f;
-    public int damage = 200;
-    protected float countdown;
-    protected bool boom;
-
-    public void Explode()
-    {
-        explosionEffect = Instantiate(explosionEffect, transform.position, transform.rotation);
-        
-        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
-
-        foreach (var collider in colliders)
-        {
-            GameObject tempGO = collider.transform.root.gameObject;
-            tempGO.GetComponent<IDamagable>()?.Svr_Damage(damage);
-            tempGO.GetComponent<IExplodable>()?.Explode();
-        }
-        Remove();
-    }
-
-    public void Remove()
-    {
-        Destroy(gameObject);
-    }
-
-	private void OnCollisionEnter(Collision collision)
+	[SerializeField] private float explosionTimer = 0;
+	public override void Start()
 	{
-		if (collision.gameObject.tag == "Zombie")
-		{
-			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX;
-			GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ;
-		}
+		base.Start();
+		GetComponent<LiveEntity>().owner = owner;
+		StartCoroutine(Explode());
+	}
+
+	//public override void OnHit(Collision collision)
+	//{
+	//	if (!isServer) return;
+	//	base.OnHit(collision);
+	//}
+
+	IEnumerator Explode()
+    {
+		yield return new WaitForSeconds(explosionTimer);
+		Svr_Explode();
+	}
+
+	[Server]
+	private void Svr_Explode()
+	{
+		GetComponent<LiveEntity>()?.Svr_DestroyEntity(transform);
 	}
 }
