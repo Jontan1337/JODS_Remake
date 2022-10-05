5e627ac5cbc77c60304dad6ce43e271e39a9f756
Jonathan Frank <jofr@pc-tec.local>
2022-10-05 08:19:13 +0200

    Read/Write for audiosource

--- Assets/Scripts/Network/CustomReadWrite.cs
+++ Assets/Scripts/Network/CustomReadWrite.cs
@@ -344,6 +344,33 @@ public static class CustomReadWrite
         }
         return identity.GetComponent<Camera>();
     }
+    public static void WriteAudioSource(this NetworkWriter writer, AudioSource value)
+    {
+        if (value == null)
+        {
+            writer.WriteUInt32(0);
+            return;
+        }
+        NetworkIdentity identity = value.GetComponent<NetworkIdentity>();
+        if (identity != null)
+        {
+            writer.WriteUInt32(identity.netId);
+        }
+        else
+        {
+            logger.LogWarning("NetworkWriter " + value + " has no NetworkIdentity");
+            writer.WriteUInt32(0);
+        }
+    }
+    public static AudioSource ReadAudioSource(this NetworkReader reader)
+    {
+        NetworkIdentity identity = reader.ReadNetworkIdentity();
+        if (identity == null)
+        {
+            return null;
+        }
+        return identity.GetComponent<AudioSource>();
+    }
 
     /*
     public static void WriteActiveSClass(this NetworkWriter writer, ActiveSClass value)
