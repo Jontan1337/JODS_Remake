71eb21ed382b331e484a6c67053f4422dba210c5
Anoobiz <36330810+Anoobiz@users.noreply.github.com>
2022-11-03 11:07:28 +0100

    Login scene + WebsocketManager

---
+++ Assets/Scripts/API stuff/WebsocketManager.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class WebsocketManager : MonoBehaviour
+{
+    public static WebsocketManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private ClientWebSocket webSocket = null;
+
+    public async Task Connect()
+    {
+        try
+        {
+            webSocket = new ClientWebSocket();
+            await webSocket.ConnectAsync(new Uri("ws://localhost:7777"), CancellationToken.None);
+
+            Debug.LogWarning(webSocket.State);
+
+            Receive();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning(ex);
+        }
+    }
+
+    private async Task Receive()
+    {
+        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[8192]);
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result;
+
+            do
+            {
+                result = await webSocket.ReceiveAsync(buffer, CancellationToken.None);
+            }
+            while (!result.EndOfMessage);
+
+            string message = System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+
+            Debug.LogWarning(message);
+        }
+    }
+
+    public async Task Send(WSRequests request)
+    {
+        try
+        {
+            //First we convert the ScoreboardRequest into a JSON object
+            //Then we encode that JSON into bytes
+            var encoded = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(request));
+            //Make an ArraySegment out of those bytes
+            ArraySegment<byte> buffer = new ArraySegment<byte>(encoded, 0, encoded.Length);
+            //Then we send the ArraySegment to the websocket.
+            await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+
+            Debug.LogWarning("message sent");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogWarning(ex);
+        }
+    }
+}
---
+++ Assets/Scripts/Main Menu/LoginManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    [SerializeField] private InputField email;
+    [SerializeField] private InputField password;
+    [SerializeField] private Text errorMessage;
+    [SerializeField] private Button loginButton;
+
+    LoginRequest loginRequest;
+
+    private void Start()
+    {
+        loginButton.onClick.AddListener(LoginBtn);
+    }
+
+    private async void LoginBtn()
+    {
+        await WebsocketManager.instance.Connect();
+
+        loginRequest = new LoginRequest()
+        {
+            type = "login",
+            email = email.text,
+            password = password.text
+        };
+
+        await WebsocketManager.instance.Send(loginRequest);
+    }
+}
